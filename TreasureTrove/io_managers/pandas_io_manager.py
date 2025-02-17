2a2c8f4ec6cfd540bbdc5e3d47704644a190dc9a
ziwei <ziwei@datastory.com.cn>
2025-02-17 15:08:49 +0800

    A股指数日线数据ingestion

--- TreasureTrove/io_managers/pandas_io_manager.py
+++ TreasureTrove/io_managers/pandas_io_manager.py
@@ -1,4 +1,6 @@
 import os
+from typing import Sequence
+
 import pandas as pd
 from dagster import (
     IOManager,
@@ -10,73 +12,51 @@ from TreasureTrove.resources import WAREHOUSE_PATH
 
 
 class CsvIOManager(IOManager):
-    def handle_output(self, context: OutputContext, obj: pd.DataFrame):
+    """
+    使用场景，单个文件，没有分区，支持增量写入
+    """
+    def __get_path(self, asset_path: Sequence[str]) -> tuple[str, str, str]:
         root_path = WAREHOUSE_PATH
-        path = context.asset_key.path
         dir_path = None
-        for dir in path[:-1]:
+        for _dir in asset_path[:-1]:
             if dir_path is None:
-                dir_path = os.path.join(root_path, dir)
+                dir_path = os.path.join(root_path, _dir)
             else:
-                dir_path = os.path.join(dir_path, dir)
-        file_name = context.asset_key.path[-1]
+                dir_path = os.path.join(dir_path, _dir)
 
-        # 检查是否存在分区键
-        partition_key = context.partition_key if context.has_partition_key else None
-        context.log.debug(f"分区键:{partition_key}")
-        if partition_key:
-            if "|" in partition_key:
-                key_dict = context.partition_key.keys_by_dimension
-                keys = list(key_dict.keys())
-                file_name_with_partition = file_name
-                for key in reversed(keys):
-                    file_name_with_partition = file_name_with_partition + '_' + key_dict[key]
-            else:
-                file_name_with_partition = f"{file_name}_{partition_key}"
+        file_name = asset_path[-1]
+        file_path = os.path.join(dir_path, f"{file_name}.csv")
+
+        return dir_path, file_name, file_path
+
+    def __column_check(self, obj: pd.DataFrame, file_path: str, context: OutputContext):
+        if os.path.exists(file_path):
+            df = pd.read_csv(file_path)
+            if set(obj.columns) != set(df.columns):
+                context.log.info(f"文件{file_path}的列名不匹配，将覆盖文件")
+                context.log.info(f"文件{file_path}的列名:{df.columns}")
+                context.log.info(f"取数结果的列名:{obj.columns}")
+                raise Exception(f"文件{file_path}的列名不匹配")
+
+    def handle_output(self, context: OutputContext, obj: pd.DataFrame):
+        dir_path, file_name, file_path = self.__get_path(context.asset_key.path)
+        # 对已存在的文件，进行增量写入
+        if os.path.exists(file_path):
+            context.log.info(f"文件{file_path}已存在，将进行增量写入")
+            df = pd.read_csv(file_path)
+            self.__column_check(obj, file_path, context)
+            df = pd.concat([df, obj], ignore_index=True)
+            return df.to_csv(file_path, index=False)
         else:
-            file_name_with_partition = file_name
-
-        if not os.path.exists(dir_path):
-            context.log.info(f"文件夹{dir_path}不存在，创建文件夹")
-            os.makedirs(dir_path)
-
-        file_path = os.path.join(dir_path, f"{file_name_with_partition}.csv")
-        context.log.info(f"输出文件:{file_name_with_partition}.csv\n到文件夹{dir_path}")
-        return obj.to_csv(file_path, index=False)
+            context.log.info(f"文件{file_path}不存在，将创建文件和文件夹")
+            os.makedirs(dir_path, exist_ok=True)
+            return obj.to_csv(file_path, index=False)
 
     def load_input(self, context: InputContext):
-        root_path = WAREHOUSE_PATH
-        path = context.upstream_output.asset_key.path
-        dir_path = None
-        for dir in path[:-1]:
-            if dir_path is None:
-                dir_path = os.path.join(root_path, dir)
-            else:
-                dir_path = os.path.join(dir_path, dir)
-
+        dir_path, file_name, file_path = self.__get_path(context.upstream_output.asset_key.path)
         if not os.path.exists(dir_path):
             raise Exception(f"文件夹{dir_path}不存在")
-
-        file_name = context.upstream_output.asset_key.path[-1]
-
-        # 检查是否存在分区键
-        partition_key = context.partition_key if context.has_partition_key else None
-        if partition_key:
-            if "|" in partition_key:
-                key_dict = context.partition_key.keys_by_dimension
-                keys = list(key_dict.keys())
-                context.log.debug(f"key: {keys}")
-                file_name_with_partition = file_name
-                for key in reversed(keys):
-                    file_name_with_partition = file_name_with_partition + '_' + key_dict[key]
-            else:
-                file_name_with_partition = f"{file_name}_{partition_key}"
-        else:
-            file_name_with_partition = file_name
-
-        file_path = os.path.join(dir_path, f"{file_name_with_partition}.csv")
-        context.log.info(f"读取文件:{file_name_with_partition}.csv \n从文件夹{dir_path}")
-
+        context.log.info(f"读取文件:{file_name}.csv \n从文件夹{dir_path}")
         return pd.read_csv(file_path)
 
 
---
+++ utils/tushare.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import tushare as ts
+from datetime import datetime, timedelta
+from typing import Tuple, Callable, Generator
+import time
+import os
+
+
+class TushareFetcher:
+    def __init__(self,
+                 fetch_func: Callable[..., pd.DataFrame],
+                 ts_codes: list[str] = None,
+                 max_rows: int = 8000,
+                 window_days: int = 90,
+                 context=None
+                 ):
+        """
+        初始化数据获取器。
+
+        :param fetch_func: 获取数据的函数，接受ts_code, start_date, end_date参数
+        :param ts_codes: 交易代码，支持输入一串
+        :param max_rows: 每批次最大获取行数
+        :param window_days: 每次查询的日期范围
+        """
+        self.fetch_func = fetch_func
+        self.ts_codes = ts_codes
+        self.ts_code_cnt = len(ts_codes)
+        self.max_rows = max_rows
+        self.window_days = window_days
+        self.context = context
+
+    def _date_range(self,
+                    start: datetime,
+                    end: datetime
+                    ) -> Generator[Tuple[datetime, datetime], None, None]:
+        """
+        生成日期范围。
+
+        :param start: 开始日期
+        :param end: 结束日期
+        :return: 生成的日期窗口(start_date, end_date)
+        """
+        # 计算每个窗口内最多允许的天数
+        allowed_days = self.max_rows
+        if allowed_days < 1:
+            raise ValueError("max_rows太小，无法容纳每个ts_code一天的数据。")
+
+        # 实际查询窗口天数不能超过初始化时设置的window_days
+        window_days = min(self.window_days, allowed_days)
+        span = timedelta(days=window_days)
+
+        current = start
+        while current < end:
+            current_end = min(current + span, end)
+            yield current, current_end
+            current = current_end + timedelta(days=1)
+
+    def single_ts_code_fetch(self,
+                             ts_code: str,
+                             start_date: str,
+                             end_date: str
+                             ) -> pd.DataFrame:
+        """
+        按ts_code, 分批获取数据。适用于ts_code不多，回溯时间长的场景
+        :param ts_code: 交易代码
+        :param start_date: 查询的起始日期
+        :param end_date: 查询的终止日期
+        :return: 获取的数据
+        """
+        start = datetime.strptime(start_date, '%Y%m%d')
+        end = datetime.strptime(end_date, '%Y%m%d')
+        self.context.log.info(f"windows \nstart_date: {start}, \nend_date: {end}")
+        results = []
+        for s, e in self._date_range(start, end):
+            self.context.log.info(f"Fetching {ts_code} data from {s} to {e}")
+            batch = self.fetch_func(ts_code=ts_code, start_date=s.strftime("%Y%m%d"), end_date=e.strftime("%Y%m%d"))
+            time.sleep(3)
+            results.append(batch)
+            self.context.log.info(f"Fetched {len(batch)} rows")
+        return pd.concat(results, ignore_index=True)
+
+
+def get_ts_source_last_trade_date_by_tscode(
+        path: str,
+        ts_codes: list[str] = None,
+        default_trade_date: str = '19910101',
+        context=None
+) -> pd.DataFrame:
+    """
+    获取tscode对应的最后交易日期
+    :param path: 存储路径
+    :param ts_codes:
+    :param default_trade_date: 默认其实日期
+    :param context: dg上下文
+    :return:
+    """
+    if not os.path.exists(path):
+        context.log.info(f"{path} 文件不存在，返回默认初始交易日")
+        trade_date_df = pd.DataFrame({
+            'ts_code': ts_codes,
+            'trade_date': default_trade_date
+        })
+    else:
+        context.log.info(f"{path} 文件存在，读取文件")
+        df = pd.read_csv(path)
+        # 如果需要，可以将trade_date转换为日期格式：
+        df['trade_date'] = pd.to_datetime(df['trade_date'], format='%Y%m%d')
+
+        # 按ts_code分组，取每组中最大的trade_date
+        df_max = df.groupby('ts_code')['trade_date'].max().reset_index()
+
+        # 只保留ts_codes中存在的记录
+        df_filtered = df_max[df_max['ts_code'].isin(ts_codes)]
+
+        # 构造包含全部ts_codes的DataFrame，并与查询结果左连接
+        trade_date_df = pd.DataFrame({'ts_code': ts_codes}).merge(df_filtered, on='ts_code', how='left')
+
+        # 对于未查到记录的ts_code，填充默认trade_date
+        trade_date_df['trade_date'] = trade_date_df['trade_date'].fillna(default_trade_date)
+        context.log.info(f"目标symbol最后交易日期\n{trade_date_df}")
+    return trade_date_df
