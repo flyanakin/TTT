23144616137972331e99b2538c7ea5842c75ea1d
ziwei <ziwei@datastory.com.cn>
2025-02-18 15:35:57 +0800

    ad_hoc query

--- TreasureTrove/assets/ad_hoc.py
+++ TreasureTrove/assets/ad_hoc.py
@@ -7,7 +7,10 @@ from dagster import (
 )
 import pandas as pd
 import os
-from TreasureTrove.resources import EnvResource
+
+from sqlalchemy.sql.functions import current_timestamp
+
+from TreasureTrove.resources import EnvResource, TushareBarsConfig
 from TreasureTrove.io_managers.pandas_io_manager import column_check, get_path, ingestion_output
 from datetime import datetime
 import tushare as ts
@@ -28,9 +31,6 @@ def au_daily(context: AssetExecutionContext, env: EnvResource):
     # tushare初始化
     ts.set_token(env.tushare_token)
     pro = ts.pro_api()
-
-    # 读取存储数据，确定每个指数的最后更新日期
-    default_trade_date = '19910101'  # 默认交易起始日期
 
     asset_key_path = context.asset_key.path
     dir_path, file_name, file_path = get_path(asset_key_path)
@@ -84,4 +84,63 @@ def au_daily(context: AssetExecutionContext, env: EnvResource):
     au_etf.to_csv(os.path.join(dir_path, 'au_etf.csv'), index=False)
 
 
+@asset(
+    group_name='Ingestion',
+    key=AssetKey(["sources", "ad_hoc", "bars"]),
+)
+def bars(context: AssetExecutionContext, env: EnvResource, config: TushareBarsConfig):
+    """
+    通用行情的日线数据
+    :param context:
+    :param env:
+    :param config: 参数配置
+            ops:
+              sources__ad_hoc__bars:
+                config:
+                  params:
+                    ts_code: ['601899.SH','600547.SH']
+                    start_date: '20000101'
+                    end_date: '20250210'
+                    asset: 'E'
+                    adj: 'hfq'
+                    freq: 'D'
+                    adjfactor: True
+                    https://tushare.pro/document/2?doc_id=109
+    :return:
+    """
+    # tushare初始化
+    ts.set_token(env.tushare_token)
+    pro = ts.pro_api()
+
+    asset_key_path = context.asset_key.path
+    dir_path, file_name, file_path = get_path(asset_key_path)
+
+    ts_codes = config.params['ts_code']
+    config.params.pop('ts_code')
+
+    daily_fetcher = TushareFetcher(
+        fetch_func=ts.pro_bar,
+        ts_codes=ts_codes,
+        window_days=1800,
+        context=context,
+        max_rows=1800,
+        params=config.params,
+    )
+
+    results = []
+    for code in ts_codes:
+        context.log.info(f"code: {code}")
+        batch = daily_fetcher.single_ts_code_fetch(
+            ts_code=code,
+            start_date=config.params['start_date'],
+            end_date=config.params['end_date'],
+        )
+        results.append(batch)
+
+    output = pd.concat(results)
+    # 按照ts_code和trade_date进行去重
+    output.drop_duplicates(subset=['ts_code', 'trade_date'], inplace=True)
+
+    now_timestamp = int(datetime.now().timestamp())
+    output.to_csv(os.path.join(dir_path, f'bars_{now_timestamp}.csv'), index=False)
 
